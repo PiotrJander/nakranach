ec7825f3bccf468a2a2d914668354014c61b6722
Marcin Struś <struchu@gmail.com>
2015-02-05 14:04:26 +0100

    NK-5 Beer model

--- app/beers/models.py
+++ app/beers/models.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class Brewery(models.Model):
+    name = models.CharField(verbose_name=_('nazwa'), max_length=255)
+    country = models.CharField(verbose_name=_('kraj'), max_length=255)
