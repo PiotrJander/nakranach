94ce62aa7fb275b73861981bdb08964dced45fa8
Marcin Struś <struchu@gmail.com>
2015-03-17 10:59:30 +0100

    NK-71 User profile

---
+++ app/users/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+    avatar_url = models.URLField(blank=True, null=True)
