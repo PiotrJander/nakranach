c814dfcd2456715cbceac035b2f32b901fb712b7
Marcin Struś <struchu@gmail.com>
2015-03-23 13:00:12 +0100

    NK-75 Favorite pub list in user profile

--- app/users/models.py
+++ app/users/models.py
@@ -3,9 +3,13 @@
 from django.conf import settings
 from django.db import models
 
+from app.pubs.models import Pub
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
     avatar_url = models.URLField(blank=True, null=True, max_length=1000)
 
     name = models.CharField(max_length=255, blank=True, null=True)
     surname = models.CharField(max_length=255, blank=True, null=True)
+
+    favorite_pubs = models.ManyToManyField(Pub)
