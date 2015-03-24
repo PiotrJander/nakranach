97808125cfa68c35657188846e4363e6b2114685
Marcin Struś <struchu@gmail.com>
2015-03-24 13:06:52 +0100

    NK-81 Location sorting

--- app/api/views/__init__.py
+++ app/api/views/__init__.py
@@ -1,7 +1,7 @@
 from .pubs import PubList, PubView, TapList, TapChangeList
 from .changes import ChangesView
 from .auth import FacebookAuthenticate, Logout, Login, Register
-from .location import NearestPubsView
+from .location import NearestPubsView, NearestActivitiesView
 
 __all__ = [
     # pub views
@@ -14,5 +14,5 @@ __all__ = [
     'Login', 'FacebookAuthenticate', 'Logout', 'Register',
 
     # location views
-    'NearestPubsView',
+    'NearestPubsView', 'NearestActivitiesView'
 ]
--- app/api/views/location.py
+++ app/api/views/location.py
@@ -5,8 +5,9 @@ from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
 from app.pubs.models import Pub
+from app.taps.models import TapChange
 
-from app.api.serializers import PubSerializer
+from app.api.serializers import PubSerializer, TapChangeSerializer
 
 MINIMAL_DISTANCE = 0.5
 MAXIMAL_DISTANCE = 10.0
@@ -22,7 +23,6 @@ class PubLocationMixin(object):
         queryset = Pub.objects.all()
 
         pubs = []
-
         buckets = [[] for _ in range(int(MINIMAL_DISTANCE * 10), int(MAXIMAL_DISTANCE * 10), int(STEP * 10))]
 
         for pub in queryset:
@@ -32,12 +32,10 @@ class PubLocationMixin(object):
                 continue
 
             index = int(pub.distance / STEP)
-
             buckets[index].append(pub)
 
         for bucket in buckets:
             sorted(bucket, key=lambda pub: pub.distance)
-
             pubs.extend(bucket)
 
             if len(pubs) >= 3:
@@ -46,10 +44,21 @@ class PubLocationMixin(object):
         return pubs
 
 class NearestPubsView(PubLocationMixin, ListAPIView):
-    queryset = Pub.objects.all()
+    queryset = Pub.objects.none()
     serializer_class = PubSerializer
 
     def list(self, request, *args, **kwargs):
         nearest_pubs = self.get_nearest_pubs()
         serializer = self.serializer_class(nearest_pubs, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class NearestActivitiesView(PubLocationMixin, ListAPIView):
+    queryset = TapChange.objects.all()
+    serializer_class = TapChangeSerializer
+
+    def list(self, request, *args, **kwargs):
+        nearest_pubs = self.get_nearest_pubs()
+
+        activities = self.queryset.filter(tap__pub__in=nearest_pubs).order_by('-timestamp')[:15]
+        serializer = self.serializer_class(activities, many=True, context={'request': request})
         return Response(serializer.data)
