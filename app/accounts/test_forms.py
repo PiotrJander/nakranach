0226d177147a20583c349bf5c29f2957f56f1e09
PiotrJander <pjander2@gmail.com>
2015-08-11 13:04:39 +0200

    Added docstrings to form tests in accounts.

--- app/accounts/test_forms.py
+++ app/accounts/test_forms.py
@@ -9,7 +9,7 @@ from app.users.models import Profile
 class TestProfileUpdateForm(TestCase):
     def setUp(self):
         super(TestProfileUpdateForm, self).setUp()
-        self.user = get_user_model().objects.create_user('a@b.com, pwd')
+        self.user = get_user_model().objects.create_user('a@b.com', 'pwd')
         self.profile = Profile.objects.create(
             name='Jan',
             surname='Nowak',
@@ -17,6 +17,9 @@ class TestProfileUpdateForm(TestCase):
         )
 
     def test_form_valid_email_changes(self):
+        """
+        The form should be valid when a new valid email is supplied.
+        """
         data = {
             'email': 'e@f.com',
             'name': 'Jan',
@@ -26,6 +29,9 @@ class TestProfileUpdateForm(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_form_valid_when_email_doesnt_change(self):
+        """
+        The form should be valid when the email doesn't change.
+        """
         data = {
             'email': 'a@b.com',
             'name': 'Jan',
@@ -35,6 +41,10 @@ class TestProfileUpdateForm(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_form_invalid_when_email_already_registered(self):
+        """
+        The form should be invalid when the new email is already registered in NaKranach.pl .
+        """
+
         # create second user
         get_user_model().objects.create_user('c@d.com', 'pwd')
 
@@ -49,6 +59,9 @@ class TestProfileUpdateForm(TestCase):
         self.assertRegexpMatches(form.errors['email'][0], ur'Email .* jest już zarejestrowany w nakranach')
 
     def test_form_save(self):
+        """
+        When data is valid, the new email should be saved to the user model instance.
+        """
         data = {
             'email': 'e@f.com',
             'name': 'Jan',
--- app/accounts/test_views.py
+++ app/accounts/test_views.py
@@ -1,14 +1,34 @@
+from django.contrib.auth import get_user_model
+
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase, RequestFactory
 
-#
-# class TestProfileRegistrationView(TestCase):
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         pass
-#
-#     def test_register(self):
-#         self.fail()
-#
-#     def test_get_context_data(self):
-#         self.fail()
+from app.accounts.views import ProfileRegistrationView
+
+
+class TestProfileRegistrationView(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = RequestFactory()
+
+    def test_post(self):
+        data = {
+            'email': 'a@a.com',
+            'name': 'Jan',
+            'surname': 'Kowalski',
+            'password1': 'pwd',
+            'password2': 'pwd',
+        }
+        request = self.factory.post('/accounts/register/', data)
+        response = ProfileRegistrationView.as_view(request)
+        try:
+            new_user = get_user_model().objects.get(email='a@a.com')
+        except ObjectDoesNotExist:
+            self.fail("User account a@a.com was not created.")
+        new_profile = new_user.profile
+        self.assertEquals(new_user.email, 'a@a.com')
+        self.assertTrue(new_user.check_password('pwd'))
+        self.assertEquals(new_profile.name, 'Jan')
+        self.assertEquals(new_profile.surname, 'Kowalski')
